04319fa3b271bd5baf504af27d0ad07700b29a44
出口友梨 <yurinco1119@gmail.com>
2022-09-30 21:16:21 +0900

    コメント機能追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,55 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
   def index
     @boards = Board.all
   end
 
   def show
+  end
+
+  def new
+    @boards = Board.new
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to board_path(@board), notice: '保存できたよ'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+
+  def edit
+    @board = current_user.boards.find(params[:id])
+  end
+
+  def update
+    @board = current_user.boards.find(params[:id])
+    if @board.update(board_params)
+      redirect_to board_path(@board), notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    board = current_user.boards.find(params[:id])
+    board.destroy!
+    redirect_to root_path, notice: '削除に成功しました'
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content, :eyecatch)
+  end
+
+  def set_board
     @board = Board.find(params[:id])
   end
 end
+
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,28 @@
 class Board < ApplicationRecord
+  validates :title, presence: true
+  validates :title, length: { minimum: 2, maximum: 100 }
+  validates :title, format: { with: /\A(?!\@)/ }
+
+  validates :content, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :content, uniqueness: true
+
+  validate :validate_title_and_content_length
+
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
+
+  def author_name
+    user.display_name
+  end
+
+  private
+  def validate_title_and_content_length
+    char_count = self.title.length + self.content.length
+    errors.add(:content, '100文字いじょうで！') unless char_count > 100
+  end
 end
